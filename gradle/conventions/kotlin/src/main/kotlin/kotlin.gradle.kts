fcaf8bdbf11e048f42de00f0b96bed1499ffb225
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-07-09 22:16:22 +0200

    build: Enable parallel test execution

--- gradle/conventions/kotlin/src/main/kotlin/kotlin.gradle.kts
+++ gradle/conventions/kotlin/src/main/kotlin/kotlin.gradle.kts
@@ -14,3 +14,7 @@ repositories {
 kotlin {
 	jvmToolchain(17)
 }
+
+tasks.withType<Test>().configureEach {
+	maxParallelForks = (Runtime.getRuntime().availableProcessors() / 2).takeIf { it > 0 } ?: 1
+}
