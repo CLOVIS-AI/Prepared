205cf0bf01a39f491cd242dfb67bc1b81778df15
Ivan “CLOVIS” Canet <ivan.canet@4sh.fr>
2023-09-06 15:14:51 +0200

    feat(framework): Avoid having an empty module

---
+++ framework/src/commonMain/kotlin/Marker.kt
@@ -0,0 +1,6 @@
+package opensavvy.prepared.framework
+
+// Kotlin/Native doesn't like modules with 0 lines of source code.
+// This is just here so the file isn't empty.
+@Suppress("unused")
+private typealias Marker = Nothing
