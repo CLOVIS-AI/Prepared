1dd6edbe00fff54ad025826282bab0324fb79b18
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-09-24 22:05:21 +0200

    build: Crash if someone forgot to change the group when importing the project

--- gradle/conventions/base/src/main/kotlin/base.gradle.kts
+++ gradle/conventions/base/src/main/kotlin/base.gradle.kts
@@ -8,3 +8,12 @@ val appVersion: String? by project
 
 group = "dev.opensavvy.playground"
 version = appVersion ?: "0.0.0-DEV"
+
+// region Check the users of the project didn't forget to rename the group
+
+val projectPath: String? = System.getenv("CI_PROJECT_PATH")
+if (projectPath != null && projectPath != "opensavvy/playgrounds/gradle" && group == "dev.opensavvy.playground") {
+	error("The project is declared to be in the group '$group', which is recognized as the Gradle Playground, but it's hosted in '$projectPath', which is not the Playground. Maybe you forgot to rename the group when importing the Playground in your own project?")
+}
+
+// endregion
