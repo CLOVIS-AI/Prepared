a8a4773b8343d7ae2905b000a0808ab13012d4f9
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-09-27 22:05:39 +0200

    build(gradle): Configure the 'core' project with the necessary metadata for Central

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -7,3 +7,9 @@ plugins {
 kotlin {
 	jvm()
 }
+
+library {
+	name.set("Playground Core")
+	description.set("Project template with configured MavenCentral publication")
+	homeUrl.set("https://gitlab.com/opensavvy/playgrounds/gradle")
+}
