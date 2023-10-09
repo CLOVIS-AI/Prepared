48f75d384c62ac6de4cdd63b1b383c095ea8a243
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-10-10 00:28:56 +0200

    docs(suite): Improve the documentation

--- suite/src/commonMain/kotlin/Finalizers.kt
+++ suite/src/commonMain/kotlin/Finalizers.kt
@@ -30,6 +30,48 @@ internal class Finalizers {
  * The block will run even if the test fails.
  *
  * Finalizers are ran in inverse order as their registration order.
+ *
+ * ### Example
+ *
+ * ```kotlin
+ * val prepareDatabase by prepared { FakeDatabase() }
+ *
+ * test("Create a user") {
+ *     val database = prepareDatabase()
+ *
+ *     val user = database.createUser()
+ *
+ *     cleanUp("Delete user $user") {
+ *         database.deleteUser(user)
+ *     }
+ * }
+ * ```
+ *
+ * ### Declaration from within prepared values
+ *
+ * This function can be called from within a [prepared] value, in which case it will run when the test that initialized that prepared value finishes:
+ * ```kotlin
+ * val prepareDatabase by prepared {
+ *     FakeDatabase()
+ *         .also { cleanUp("Disconnect from the database") { it.disconnect() } }
+ * }
+ *
+ * val prepareUser by prepared {
+ *     val database = prepareDatabase()
+ *     database.createUser()
+ *         .also { cleanUp("Delete user $it") { database.deleteUser(it) } }
+ * }
+ *
+ * test("Rename a user") {
+ *     val user = prepareUser()
+ *
+ *     user.rename("New name")
+ *
+ *     // will automatically run:
+ *     // 1. Delete user …
+ *     // 2. Disconnect from the database
+ * }
+ * ```
  */
 @PreparedDslMarker
 suspend fun TestDsl.cleanUp(name: String, block: suspend TestDsl.() -> Unit) {
