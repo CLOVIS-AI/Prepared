ad6408e2eba4fc3a8244372c8ab6185b971849ec
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-11-08 12:57:24 +0100

    refactor(compat-arrow): Allow using failOnRaise outside the TestDSL

--- compat/compat-arrow/src/commonMain/kotlin/FailOnRaise.kt
+++ compat/compat-arrow/src/commonMain/kotlin/FailOnRaise.kt
@@ -5,15 +5,13 @@ import arrow.core.raise.ExperimentalTraceApi
 import arrow.core.raise.Raise
 import arrow.core.raise.either
 import opensavvy.prepared.suite.PreparedDslMarker
-import opensavvy.prepared.suite.TestDsl
 
 /**
  * Fails the test if [block] raises.
  */
-@Suppress("UnusedReceiverParameter") // we're keeping the receiver for scoping
 @ExperimentalTraceApi
 @PreparedDslMarker
-inline fun <Failure, Success> TestDsl.failOnRaise(block: Raise<Failure>.() -> Success): Success {
+inline fun <Failure, Success> failOnRaise(block: Raise<Failure>.() -> Success): Success {
 	return either(block)
 		.getOrElse { throw AssertionError("Expected the block to execute successfully, but a value was raised: $it") }
 }
