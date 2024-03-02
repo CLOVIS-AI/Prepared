344aa7fced87f62470c7b160a025abc6986fd602
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2024-03-02 12:44:01 +0100

    test(compat-arrow): Use PreparedSpec in the tests

--- compat/compat-arrow/src/commonTest/kotlin/FailOnRaiseTest.kt
+++ compat/compat-arrow/src/commonTest/kotlin/FailOnRaiseTest.kt
@@ -2,20 +2,18 @@ package opensavvy.prepared.compat.arrow.core
 
 import arrow.core.raise.ExperimentalTraceApi
 import io.kotest.assertions.throwables.shouldThrow
-import io.kotest.core.spec.style.StringSpec
-import opensavvy.prepared.runner.kotest.preparedSuite
+import opensavvy.prepared.runner.kotest.PreparedSpec
 
+@Suppress("unused")
 @OptIn(ExperimentalTraceApi::class)
-class FailOnRaiseTest : StringSpec({
-	preparedSuite {
-		test("Successful operation") {
-			failOnRaise<Nothing, Int> { 5 }
-		}
+class FailOnRaiseTest : PreparedSpec({
+	test("Successful operation") {
+		failOnRaise<Nothing, Int> { 5 }
+	}
 
-		test("Failed operation") {
-			shouldThrow<AssertionError> {
-				failOnRaise { raise(5) }
-			}
+	test("Failed operation") {
+		shouldThrow<AssertionError> {
+			failOnRaise { raise(5) }
 		}
 	}
 })
