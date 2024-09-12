763da96e0535739536f13e4039201b61977b23f0
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2024-09-12 12:02:42 +0200

    feat(compat-arrow): Support linuxArm64, iOS, macOS, watchOS, tvOS, mingwX64

--- compat/compat-arrow/build.gradle.kts
+++ compat/compat-arrow/build.gradle.kts
@@ -17,9 +17,20 @@ kotlin {
 		browser()
 	}
 	linuxX64()
+	linuxArm64()
+	macosX64()
+	macosArm64()
 	iosArm64()
 	iosSimulatorArm64()
 	iosX64()
+	watchosX64()
+	watchosArm32()
+	watchosArm64()
+	watchosSimulatorArm64()
+	tvosX64()
+	tvosArm64()
+	tvosSimulatorArm64()
+	mingwX64()
 
 	val commonMain by sourceSets.getting {
 		dependencies {
