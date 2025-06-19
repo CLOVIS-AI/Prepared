3ff2465d8682bec09d44a6b88074513e208b773a
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2025-06-19 18:33:09 +0200

    test(compat-parameterize): Migrate from Kotest Runner to TestBalloon runner

--- compat/compat-parameterize/src/commonTest/kotlin/ParameterizeExamples.kt
+++ compat/compat-parameterize/src/commonTest/kotlin/ParameterizeExamples.kt
@@ -2,30 +2,15 @@ package opensavvy.prepared.compat.parameterize
 
 import com.benwoodworth.parameterize.parameterOf
 import com.benwoodworth.parameterize.parameterize
-import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.shouldBe
-import opensavvy.prepared.runner.kotest.preparedSuite
+import opensavvy.prepared.runner.testballoon.preparedSuite
 
-class ParameterizeExamples : StringSpec({
-	// Regular Kotest example
+val ParameterizeExamples by preparedSuite {
 	parameterize {
 		val text by parameterOf("123456", "hello64")
 		val int by parameterOf(4, 6)
 
-		"Kotest $text $int" {
-			(int.toString() in text) shouldBe true
+		test("Prepared $text $int") {
+			check(int.toString() in text)
 		}
 	}
-
-	// The same example with Prepared syntax
-	preparedSuite {
-		parameterize {
-			val text by parameterOf("123456", "hello64")
-			val int by parameterOf(4, 6)
-
-			test("Prepared $text $int") {
-				(int.toString() in text) shouldBe true
-			}
-		}
-	}
-})
+}
