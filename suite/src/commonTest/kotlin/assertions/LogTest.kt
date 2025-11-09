16df295be0437c246c7d77683d4e8d9cce75a3f6
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2025-11-09 11:48:16 +0100

    feat(suite): Create the function 'log' to easily log intermediate values

---
+++ suite/src/commonTest/kotlin/assertions/LogTest.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2025, OpenSavvy and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package opensavvy.prepared.suite.assertions
+
+import opensavvy.prepared.runner.testballoon.preparedSuite
+
+val LogTest by preparedSuite {
+	test("Without message") {
+		log(12 + 5)
+	}
+
+	test("With message") {
+		log(12 + 5) { "The result of 12 + 5" }
+	}
+}
