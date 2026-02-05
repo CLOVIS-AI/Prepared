49dd012800891861cc8a97292b0dc0ae776d9a3e
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2026-02-05 16:18:47 +0100

    build(template-app): Force the Kotest plugin and engine versions

--- gradle/templates/template-app/build.gradle.kts
+++ gradle/templates/template-app/build.gradle.kts
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2026, OpenSavvy and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 @file:OptIn(ExperimentalWasmDsl::class, ExperimentalKotlinGradlePluginApi::class)
 
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
@@ -7,6 +23,7 @@ plugins {
 	alias(opensavvyConventions.plugins.base)
 	alias(opensavvyConventions.plugins.kotlin.application)
 	alias(libsCommon.plugins.ksp)
+	alias(libsCommon.plugins.kotest)
 }
 
 kotlin {
@@ -47,5 +64,7 @@ kotlin {
 
 	sourceSets.commonTest.dependencies {
 		implementation(libsCommon.opensavvy.prepared.kotest)
+		implementation(libsCommon.kotest.engine)
+		implementation(libsCommon.kotest.assertions)
 	}
 }
