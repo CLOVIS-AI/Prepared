43ef50cb391f52e042f1d9c1354bc78f55f22fe5
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2026-02-15 12:17:08 +0100

    ci(gitlab): Test that our tests can deal with Unicode characters in test names

--- gradle/templates/template-app/src/commonTest/kotlin/HelloWorldKotest.kt
+++ gradle/templates/template-app/src/commonTest/kotlin/HelloWorldKotest.kt
@@ -9,6 +9,11 @@ class HelloWorldKotestTest : PreparedSpec({
 		@Suppress("SimplifyBooleanWithConstants") // The compiler knows this test will always succeed
 		check("Hello World!" == message)
 	}
+
+	test("Yay • \u2606\u2606\u2606\u2606 Unicode!") {
+		// This test verifies that our CI is able to handle Unicode characters in test names.
+		// See https://gitlab.com/gitlab-org/gitlab/-/issues/580885
+	}
 })
 
 class FooTest : FunSpec({
