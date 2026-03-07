fddb0e02812d2ddc714616e47c0eaa7652082794
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2026-03-07 22:38:20 +0100

    refactor(compat-arrow): Restructure the folders of the :compat-arrow module
    This commit does not change the published binary in any way.

--- compat/compat-arrow/src/commonMain/kotlin/Marker.kt
+++ compat/compat-arrow/src/commonMain/kotlin/Marker.kt
@@ -15,23 +15,3 @@
  */
 
 package opensavvy.prepared.compat.arrow
-
-import arrow.core.raise.ExperimentalTraceApi
-import opensavvy.prepared.compat.arrow.core.failOnRaise
-import opensavvy.prepared.runner.testballoon.preparedSuite
-
-@Suppress("unused")
-@OptIn(ExperimentalTraceApi::class)
-val FailOnRaiseTest by preparedSuite {
-	test("Successful operation") {
-		failOnRaise<Nothing, Int> { 5 }
-	}
-
-	test("Failed operation") {
-		try {
-			failOnRaise { raise(5) }
-			error("Should have thrown an AssertionError")
-		} catch (e: AssertionError) {
-		}
-	}
-}
