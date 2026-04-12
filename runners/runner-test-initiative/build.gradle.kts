80c5f0b6a11214c66681aa0b6469419800773091
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2026-04-12 15:58:31 +0200

    build(gradle): Remove Intel-based Apple targets
    Apple is deprecating Intel-based architectures.
    
    See https://youtrack.jetbrains.com/issue/KT-78660

--- runners/runner-test-initiative/build.gradle.kts
+++ runners/runner-test-initiative/build.gradle.kts
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2026, OpenSavvy and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 @file:OptIn(ExperimentalWasmDsl::class)
 
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
@@ -15,16 +31,12 @@ kotlin {
 	}
 	linuxX64()
 	linuxArm64()
-	macosX64()
 	macosArm64()
 	iosArm64()
-	iosX64()
 	iosSimulatorArm64()
-	watchosX64()
 	watchosArm32()
 	watchosArm64()
 	watchosSimulatorArm64()
-	tvosX64()
 	tvosArm64()
 	tvosSimulatorArm64()
 	mingwX64()
